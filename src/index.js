96d7659c5b01b2287c638f7c956ad8af5b554599
Jack Sarles <57759984+jsarles-code@users.noreply.github.com>
2024-03-07 06:46:43 -0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
 console.log('Webpack is working!');
+import loadHome from './home';
+// Future imports for other tabs like import loadMenu from './menu';
+
+document.addEventListener('DOMContentLoaded', () => {
+  loadHome(); // Load the home page content by default
+});
+
+// Event listeners for each tab
+document.getElementById('home').addEventListener('click', loadHome);
+// Future event listeners for other tabs
+// document.getElementById('menu').addEventListener('click', loadMenu);
